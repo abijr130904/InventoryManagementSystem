8974d84966066fb5f0145ea2d923615d4e2d0d07
Habiburrohman <149951809+abijr130904@users.noreply.github.com>
2025-08-23 11:37:46 +0700

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,6 +20,7 @@ class ProductSeeder extends Seeder
                 'harga_beli' => 8000000,
                 'harga_jual' => 9500000,
                 'stok' => 10,
+                'satuan' => 'pcs',
             ],
             [
                 'kode_produk' => 'P002',
@@ -28,6 +29,7 @@ class ProductSeeder extends Seeder
                 'harga_beli' => 3000000,
                 'harga_jual' => 3500000,
                 'stok' => 15,
+                'satuan' => 'pcs',
             ],
             [
                 'kode_produk' => 'P003',
@@ -36,6 +38,25 @@ class ProductSeeder extends Seeder
                 'harga_beli' => 150000,
                 'harga_jual' => 250000,
                 'stok' => 25,
+                'satuan' => 'pcs',
+            ],
+            [
+                'kode_produk' => 'P004',
+                'nama_produk' => 'Beras Premium',
+                'kategori' => 'Makanan',
+                'harga_beli' => 12000,
+                'harga_jual' => 15000,
+                'stok' => 100,
+                'satuan' => 'kg',
+            ],
+            [
+                'kode_produk' => 'P005',
+                'nama_produk' => 'Minyak Goreng',
+                'kategori' => 'Makanan',
+                'harga_beli' => 18000,
+                'harga_jual' => 22000,
+                'stok' => 50,
+                'satuan' => 'liter',
             ],
         ];
 
